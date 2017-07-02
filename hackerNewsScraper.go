b4c1673be82b6461b0cf2df6880643432fcba11d
kade killary <kadekillary@protonmail.com>
2017-07-01 19:56:03 -0700

    scrape hacker news front page

---
+++ hackerNewsScraper.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yhat/scrape"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"net/http"
+)
+
+func main() {
+
+	// request and parse front page
+	resp, err := http.Get("https://news.ycombinator.com/")
+	if err != nil {
+		panic(err)
+	}
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// define match
+	matcher := func(n *html.Node) bool {
+		if n.DataAtom == atom.A && n.Parent != nil && n.Parent.Parent != nil {
+			return scrape.Attr(n.Parent.Parent, "class") == "athing"
+		}
+		return false
+	}
+	articles := scrape.FindAll(root, matcher)
+	for i, article := range articles {
+		fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
+	}
+}
